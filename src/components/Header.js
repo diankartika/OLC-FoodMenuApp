54cb24133c5ee2f705ba9dea901edac9268a9cb6
diankartika <diankartika487@gmail.com>
2024-10-30 19:39:25 +0700

    New components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Search recipe - Theme 1</h1>
+      <button>Change Theme</button>
+    </header>
+  );
+};
+
+export default Header;
